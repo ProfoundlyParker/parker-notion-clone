08b190437ed5a32ed80ac1a00be1f2a9faab33e4
ProfoundlyParker <parkerramey@yahoo.com>
2024-03-14 21:05:15 -0400

    Adding Page component to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
 import { Cover } from './Page/Cover'
+import { Page } from './Page/Page'
 
 function App() {
   return (
     <>
-      <Cover />
+      {/* <Cover /> */}
+      <Page />
     </>
   )
 }
