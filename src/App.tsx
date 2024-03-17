ad9e71998e0fb424b5e06b27ea8e71e322161bd0
ProfoundlyParker <parkerramey@yahoo.com>
2024-03-17 18:21:11 -0400

    Added routing syntax to add user authentication

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,28 @@ import './App.css';
 import { AppStateProvider } from './state/AppStateContext';
 import { Page } from './Page/Page';
 import { createPage } from './utils/createPage';
+import { Route, Routes } from 'react-router-dom';
 
 const initialState = createPage()
+
+const Auth = () => <div>Auth</div>
 
 function App() {
   return (
     <>
-    <AppStateProvider initialState={initialState}>
-      <Page />
-    </AppStateProvider>
+    <Routes>
+      <Route path="/auth" element={<Auth/>} />
+      <Route path="/:id" element={
+        <AppStateProvider initialState={initialState}>
+          <Page />
+        </AppStateProvider>
+      } />
+      <Route path="/" element={
+        <AppStateProvider initialState={initialState}>
+          <Page />
+        </AppStateProvider>
+      } />
+    </Routes>
     </>
   )
 }
