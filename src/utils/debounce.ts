88654c43ca841e2e5f9fe29770c9e655f66039a0
ProfoundlyParker <parkerramey@yahoo.com>
2024-03-25 17:46:34 -0400

    Added ESLint ignore for any types in this file

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/ban-types */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 type ArgumentTypes<F extends Function> = F extends (...args: infer A) => any
 	? A
 	: never;
