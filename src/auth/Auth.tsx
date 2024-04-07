3e709867199801d7cdd9d7de4ed0286f9ebe5631
ProfoundlyParker <parkerramey@yahoo.com>
2024-04-07 16:39:59 -0400

    Added error handling so signin errors are readable

--- src/auth/Auth.tsx
+++ src/auth/Auth.tsx
@@ -4,25 +4,35 @@ import { Navigate } from "react-router-dom";
 import styles from "../utils.module.css";
 import { supabase } from "../supabaseClient";
 
+type SignInErrorProps = {
+    message: string;
+}
+
 export const Auth = () => {
     const [ loading, setLoading ] = useState(false);
     const [ email, setEmail ] = useState("");
     const { session } = useAuthSession();
+    const [ error, setError ] = useState<string | null>(null);
 
     const handleLogin = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        setLoading(true);
+        setError(null);
 
         try {
-            setLoading(true)
             const { error } = await supabase.auth.signInWithOtp({ email });
 
             if (error) {
-                throw Error
+                throw new Error(error.message);
             }
             alert("Check your email for the login link");
-        }
-        catch (error) {
-            alert(error)
+        } catch (error) {
+            if (typeof error === 'string') {
+                setError(error);
+            } else {
+                const signInError = error as SignInErrorProps;
+                setError(signInError.message || "An unknown error occurred"); // Set error message in state
+            }
         } finally {
             setLoading(false);
         }
@@ -37,6 +47,7 @@ export const Auth = () => {
             <div>
                 <h1>Parker's Notion Clone</h1>
                 <p>Sign in via login link with your email below</p>
+                {error && <p>{error}</p>}
                 {loading ? ("Sending login link...") : (
                     <form onSubmit={handleLogin}>
                         <label htmlFor="email">Email: </label>
