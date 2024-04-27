2c452483b811c087b2904f1eaace57ea262ded90
ProfoundlyParker <parkerramey@yahoo.com>
2024-04-27 16:35:19 -0400

    Disabled React strict mode for prod

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
@@ -6,11 +5,9 @@ import { BrowserRouter } from 'react-router-dom'
 import { AuthSessionProvider } from './auth/AuthSessionContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
     <BrowserRouter>
       <AuthSessionProvider>
         <App />
       </AuthSessionProvider>
     </BrowserRouter>
-  </React.StrictMode>,
 )
